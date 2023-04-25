003deef230cc3b7bc28a07c1dff2b772103be345
soha <soha@idp-corp.com>
2023-04-25 15:40:32 +0900

    ios ui 진행 중

--- lib/view/material/main_material_view.dart
+++ lib/view/material/main_material_view.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import '../../provider/main_provider.dart';
 
 
 class MaterialMainPage extends StatefulWidget {
@@ -21,10 +24,97 @@ class _MaterialMainPageState extends State<MaterialMainPage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text(''),
-        ),
+          minimum: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              _ButtonMenu(),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: _MessageView(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  child: Text('clear'),
+                  onPressed: () {
+                    context.read<MainProvider>().clearMessage();
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          )
       ),
     );
+  }
+}
+
+class _ButtonMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+          child: Text('open Server'),
+          onPressed: () {
+            context.read<MainProvider>().openServer();
+          },
+        ),
+        ElevatedButton(
+          child: Text('close Server'),
+          onPressed: () {
+            context.read<MainProvider>().closeServer();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _MessageView extends StatelessWidget {
+  ScrollController controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    final msg = context.select((MainProvider provider) => provider.message);
+    _scrolling();
+
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            color: Colors.black,
+            width: 0.5,
+          )
+      ),
+      child: SingleChildScrollView(
+        controller: controller,
+        child: Text(msg),
+      ),
+    );
+  }
+
+  _scrolling() {
+    if(controller.positions.isNotEmpty) {
+      controller.animateTo(
+        controller.position.maxScrollExtent,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 }
